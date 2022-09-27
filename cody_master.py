82afce09127202ae724b995df7222d7e21658be7
Edoardo Cantagallo <44615027+Edoardoba@users.noreply.github.com>
2022-09-27 21:09:43 +0200

    Update cody_master.py

--- cody_master.py
+++ cody_master.py
@@ -43,7 +43,7 @@ if not creds or not creds.valid:
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
+        creds = flow.run_local_server(port=8080)
 
 
 
