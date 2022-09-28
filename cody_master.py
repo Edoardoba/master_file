d4428f315ffc7824e6656bfd2a1b5abc163de29f
Edoardo Cantagallo <44615027+Edoardoba@users.noreply.github.com>
2022-09-28 19:08:52 +0200

    Update cody_master.py

--- cody_master.py
+++ cody_master.py
@@ -11,6 +11,65 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2 import *
 
 
+class DriveAPI:
+    global SCOPES
+
+    # Define the scopes
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+
+    def __init__(self):
+
+        # Variable self.creds will
+        # store the user access token.
+        # If no valid token found
+        # we will create one.
+        self.creds = None
+        self.lists={}
+
+        # The file token.pickle stores the
+        # user's access and refresh tokens. It is
+        # created automatically when the authorization
+        # flow completes for the first time.
+
+        # Check if file token.pickle exists
+        if os.path.exists('token.pickle'):
+            # Read the token from the file and
+            # store it in the variable self.creds
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+
+        # If no valid credentials are available,
+        # request the user to log in.
+        if not self.creds or not self.creds.valid:
+
+            # If token is expired, it will be refreshed,
+            # else, we will request a new one.
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+
+            # Save the access token in token.pickle
+            # file for future usage
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.creds, token)
+
+        # Connect to the API service
+        self.service = build('drive', 'v3', credentials=self.creds)
+
+        # request a list of first N files or
+        # folders with name and id from the API.
+        results = self.service.files().list(
+            pageSize=100).execute()
+        items = results.get('files', [])
+        self.lists=items
+        # print a list of files
+
+        print("Here's a list of files: \n")
+        print(*items, sep="\n", end="\n\n")
+
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
@@ -27,39 +86,10 @@ def to_excel(df):
 
 st.title('Master File Tool')
 
+obj = DriveAPI()
+required_lists_of_files_with_id=obj.lists
+st.write(required_lists_of_files_with_id)
 
-SCOPES = ['https://www.googleapis.com/auth/drive']
-
-creds = None
-
-
-# request the user to log in.
-if not creds or not creds.valid:
-
-    # If token is expired, it will be refreshed,
-    # else, we will request a new one.
-    if creds and self.creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=8080)
-
-
-
-# Connect to the API service
-service = build('drive', 'v3', credentials=creds)
-
-# request a list of first N files or
-# folders with name and id from the API.
-results = service.files().list(
-    pageSize=100, fields="files(id, name)").execute()
-items = results.get('files', [])
-
-# print a list of files
-
-print("Here's a list of files: \n")
-print(*items, sep="\n", end="\n\n")
 
 st.write('OKKK')
 
